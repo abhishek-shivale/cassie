f13d82732fdc8c444193a5b4c1222238d744c8e9
Abhishek shivale <abhishekshivale45@gmail.com>
2026-06-01 17:49:20 +0530

    feat: started working on cassie

---
+++ programs/cassie/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5sU2QBvow11aj1m6z6DdqpsaqVuh84e8RWpQD5njdgYM");
+
+#[program]
+pub mod cassie {
+    use super::*;
+
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     initialize::handler(ctx)
+    // }
+}
